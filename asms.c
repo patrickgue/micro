fbc5519a381def09fd7e407bde5e25b7132a1dcd
patrickgue <me@patrickguenthard.ch>
2019-07-23 22:36:57 +0200

    tried to create a common code base for assemblers. Not yet working (segfault...)

--- asms.c
+++ asms.c
@@ -3,22 +3,31 @@
 #include <string.h>
 #include <inttypes.h>
 
+#include "asmhelper.h"
+
 int main(int argc, char **argv)
 {
   long input_file_size;
   size_t input_file_size_actual;
   char *buffer, *token_line, *token;
-  
+
+  FILE *input_file;
+  FILE *output_file;
+
+  /*if(!init_file_pointers(input_file, output_file, argc, argv)) {
+    return 1;
+    }*/
   if(argc < 3) {
     printf("Not enough arguments\n");
     return 1;
   }
 
-  FILE *input_file =  fopen(argv[1], "r");
-  FILE *output_file = fopen(argv[2], "wb");
+  input_file = fopen(argv[1], "r");
+  output_file = fopen(argv[2], "wb");
 
   if (!input_file || !output_file) {
     printf("Error opening files");
+    return 1;
   }
   
   fseek (input_file , 0 , SEEK_END);
